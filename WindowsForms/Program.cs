c6ceeac8b8ac4dc77eb787362adb343c426157ec
nguyencuong382 <nguyenmanhcuong382@gmail.com>
2019-03-14 17:21:35 +0700

    update c#

--- WindowsForms/Program.cs
+++ WindowsForms/Program.cs
@@ -16,7 +16,8 @@ namespace WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new DataSource_());
         }
     }
 }
